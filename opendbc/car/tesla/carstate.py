3d99b81c67f47287cc71047ddae221ba803532dd
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2025-12-22 10:46:12 -0800

    dynamic hands on level for steering disengage

--- opendbc/car/tesla/carstate.py
+++ opendbc/car/tesla/carstate.py
@@ -18,8 +18,11 @@ class CarState(CarStateBase):
     self.autopark_prev = False
     self.cruise_enabled_prev = False
 
-    self.hands_on_level = 0
+    self.c_frame = 0
+    self.blinkers = 0
     self.das_control = None
+    self.road_noise = False
+    self._steer_torque_hist = []
 
   def update_autopark_state(self, autopark_state: str, cruise_enabled: bool):
     autopark_now = autopark_state in ("ACTIVE", "COMPLETE", "SELFPARK_STARTED")
@@ -54,6 +57,30 @@ class CarState(CarStateBase):
     ret.steeringRateDeg = -cp_ap_party.vl["SCCM_steeringAngleSensor"]["SCCM_steeringAngleSpeed"]
     ret.steeringTorque = -epas_status["EPAS3S_torsionBarTorque"]
 
+    # Road noise detection
+    self._steer_torque_hist.append(ret.steeringTorque)
+    if len(self._steer_torque_hist) > 30:  # 30 samples @ 100Hz ~300ms
+      self._steer_torque_hist.pop(0)
+
+    self.road_noise = False
+    if len(self._steer_torque_hist) >= 30:
+      torque = self._steer_torque_hist
+      mean = sum(torque) / len(torque)
+      var = sum((t - mean) ** 2 for t in torque) / len(torque)
+      std = var ** 0.5
+
+      zero_crossings = sum(
+        1 for i in range(1, len(torque))
+        if torque[i - 1] * torque[i] < 0
+      ) / (len(torque) - 1)
+
+      self.road_noise = (
+        abs(mean) < 0.1 and   # no sustained driver input
+        std > 0.5 and         # noisy torque
+        zero_crossings > 0.3  # rapidly alternating
+      )
+
+
     # stock handsOnLevel uses >0.5 for 0.25s, but is too slow
     ret.steeringPressed = self.update_steering_pressed(abs(ret.steeringTorque) > STEER_THRESHOLD, 5)
 
@@ -63,8 +90,8 @@ class CarState(CarStateBase):
 
     # FSD disengages using union of handsOnLevel (slow overrides) and high angle rate faults (fast overrides, high speed)
     eac_error_code = self.can_define.dv["EPAS3S_sysStatus"]["EPAS3S_eacErrorCode"].get(int(epas_status["EPAS3S_eacErrorCode"]), None)
-    ret.steeringDisengage = self.hands_on_level >= 1 or (eac_status == "EAC_INHIBITED" and
-                                                         eac_error_code == "EAC_ERROR_HIGH_ANGLE_RATE_SAFETY")
+    ret.steeringDisengage = self.update_handsOnDisengage(cruise_state) or \
+                                  (eac_status == "EAC_INHIBITED" and eac_error_code == "EAC_ERROR_HIGH_ANGLE_RATE_SAFETY")
 
     # Cruise state
     cruise_state = self.can_define.dv["DI_state"]["DI_cruiseState"].get(int(cp_party.vl["DI_state"]["DI_cruiseState"]), None)
@@ -72,6 +99,7 @@ class CarState(CarStateBase):
 
     autopark_state = self.can_define.dv["DI_state"]["DI_autoparkState"].get(int(cp_party.vl["DI_state"]["DI_autoparkState"]), None)
     cruise_enabled = cruise_state in ("ENABLED", "STANDSTILL", "OVERRIDE", "PRE_FAULT", "PRE_CANCEL")
+    self.c_frame = (self.c_frame + 1) * int(cruise_enabled)
     self.update_autopark_state(autopark_state, cruise_enabled)
 
     # Match panda safety cruise engaged logic
@@ -94,6 +122,7 @@ class CarState(CarStateBase):
     # Blinkers
     ret.leftBlinker = cp_party.vl["UI_warning"]["leftBlinkerBlinking"] in (1, 2)
     ret.rightBlinker = cp_party.vl["UI_warning"]["rightBlinkerBlinking"] in (1, 2)
+    self.blinkers = ret.leftBlinker or ret.rightBlinker
 
     # Seatbelt
     ret.seatbeltUnlatched = cp_party.vl["UI_warning"]["buckleStatus"] != 1
@@ -119,6 +148,22 @@ class CarState(CarStateBase):
     self.das_control = copy.copy(cp_ap_party.vl["DAS_control"])
 
     return ret, ret_sp
+
+  def update_handsOnDisengage(self, cruise_state):
+    # dynamic hands on level to disable.
+    # L3: blinkers or standstill
+    # L2: recent cruise engagement or road noise
+    # L1: normal driving only if nothing else prevents it
+    standstill = cruise_state == "STANDSTILL"
+
+    if self.blinkers or standstill:
+      min_level = 3
+    elif self.c_frame <= 100 or self.road_noise:
+      min_level = 2
+    else:
+      min_level = 1
+
+    return self.hands_on_level >= min_level
 
   @staticmethod
   def get_can_parsers(CP, CP_SP):
