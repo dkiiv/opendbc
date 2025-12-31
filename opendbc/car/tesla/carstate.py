7cbb7bce0442dc72a8580b291741f7542784aa2a
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2025-12-31 09:00:30 -0800

    the holy red diff; remove unneeded features * remove road noise detection, and dynamic hands on level for steering disengage * remove long jerk bandaid, fix needs to be applied in openpilot not opendbc

--- opendbc/car/tesla/carstate.py
+++ opendbc/car/tesla/carstate.py
@@ -18,14 +18,8 @@ class CarState(CarStateBase):
     self.autopark_prev = False
     self.cruise_enabled_prev = False
 
-    self.c_frame = 0
-    self.blinkers = 0
     self.das_control = None
     self.das_accCancel = False
-    self.road_noise = False
-    self.gasPressed = False
-    self.speedGate = False
-    self._steer_torque_hist = []
 
   def update_autopark_state(self, autopark_state: str, cruise_enabled: bool):
     autopark_now = autopark_state in ("ACTIVE", "COMPLETE", "SELFPARK_STARTED")
@@ -48,7 +42,6 @@ class CarState(CarStateBase):
 
     # Gas pedal
     ret.gasPressed = cp_party.vl["DI_systemStatus"]["DI_accelPedalPos"] > 0
-    self.gasPressed = ret.gasPressed
 
     # Brake pedal
     ret.brake = 0
@@ -60,30 +53,6 @@ class CarState(CarStateBase):
     ret.steeringAngleDeg = -epas_status["EPAS3S_internalSAS"]
     ret.steeringRateDeg = -cp_ap_party.vl["SCCM_steeringAngleSensor"]["SCCM_steeringAngleSpeed"]
     ret.steeringTorque = -epas_status["EPAS3S_torsionBarTorque"]
-
-    # Road noise detection
-    self._steer_torque_hist.append(ret.steeringTorque)
-    if len(self._steer_torque_hist) > 30:  # 30 samples @ 100Hz ~300ms
-      self._steer_torque_hist.pop(0)
-
-    self.road_noise = False
-    if len(self._steer_torque_hist) >= 30:
-      torque = self._steer_torque_hist
-      mean = sum(torque) / len(torque)
-      var = sum((t - mean) ** 2 for t in torque) / len(torque)
-      std = var ** 0.5
-
-      zero_crossings = sum(
-        1 for i in range(1, len(torque))
-        if torque[i - 1] * torque[i] < 0
-      ) / (len(torque) - 1)
-
-      self.road_noise = (
-        abs(mean) < 0.1 and   # no sustained driver input
-        std > 0.5 and         # noisy torque
-        zero_crossings > 0.3  # rapidly alternating
-      )
-
 
     # stock handsOnLevel uses >0.5 for 0.25s, but is too slow
     ret.steeringPressed = self.update_steering_pressed(abs(ret.steeringTorque) > STEER_THRESHOLD, 5)
@@ -99,23 +68,19 @@ class CarState(CarStateBase):
 
     # FSD disengages using union of handsOnLevel (slow overrides) and high angle rate faults (fast overrides, high speed)
     eac_error_code = self.can_define.dv["EPAS3S_sysStatus"]["EPAS3S_eacErrorCode"].get(int(epas_status["EPAS3S_eacErrorCode"]), None)
-    ret.steeringDisengage = self.update_handsOnDisengage(cruise_state) or \
-                                  (eac_status == "EAC_INHIBITED" and eac_error_code == "EAC_ERROR_HIGH_ANGLE_RATE_SAFETY")
+    ret.steeringDisengage = self.hands_on_level >= 3 or (eac_status == "EAC_INHIBITED" and
+                                                      eac_error_code == "EAC_ERROR_HIGH_ANGLE_RATE_SAFETY")
 
     autopark_state = self.can_define.dv["DI_state"]["DI_autoparkState"].get(int(cp_party.vl["DI_state"]["DI_autoparkState"]), None)
     cruise_enabled = cruise_state in ("ENABLED", "STANDSTILL", "OVERRIDE", "PRE_FAULT", "PRE_CANCEL")
-    self.c_frame = (self.c_frame + 1) * (eac_status in ("EAC_ACTIVE", "EMERGENCY_LANE_KEEP") and \
-                                          self.cruise_enabled_prev == cruise_enabled)  # reset c_frame on rising/falling edge of cruise (button press)
     self.update_autopark_state(autopark_state, cruise_enabled)
 
     # Match panda safety cruise engaged logic
     ret.cruiseState.enabled = cruise_enabled and not self.autopark
     if speed_units == "KPH":
-      ret.cruiseState.speed = self.cruise_state_speed(max(cp_party.vl["DI_state"]["DI_digitalSpeed"] * CV.KPH_TO_MS, 1e-3), \
-                                                      max(cp_party.vl["DI_speed"]["DI_vehicleSpeed"] * CV.KPH_TO_MS, 1e-3))
+      ret.cruiseState.speed = max(cp_party.vl["DI_state"]["DI_digitalSpeed"] * CV.KPH_TO_MS, 1e-3)
     elif speed_units == "MPH":
-      ret.cruiseState.speed = self.cruise_state_speed(max(cp_party.vl["DI_state"]["DI_digitalSpeed"] * CV.MPH_TO_MS, 1e-3), \
-                                                      max(cp_party.vl["DI_speed"]["DI_vehicleSpeed"] * CV.KPH_TO_MS, 1e-3))
+      ret.cruiseState.speed = max(cp_party.vl["DI_state"]["DI_digitalSpeed"] * CV.MPH_TO_MS, 1e-3)
     ret.cruiseState.available = cruise_state == "STANDBY" or ret.cruiseState.enabled
     ret.cruiseState.standstill = False  # This needs to be false, since we can resume from stop without sending anything special
     ret.standstill = cruise_state == "STANDSTILL"
@@ -130,7 +95,6 @@ class CarState(CarStateBase):
     # Blinkers
     ret.leftBlinker = cp_party.vl["UI_warning"]["leftBlinkerBlinking"] in (1, 2)
     ret.rightBlinker = cp_party.vl["UI_warning"]["rightBlinkerBlinking"] in (1, 2)
-    self.blinkers = ret.leftBlinker or ret.rightBlinker
 
     # Seatbelt
     ret.seatbeltUnlatched = cp_party.vl["UI_warning"]["buckleStatus"] != 1
@@ -156,38 +120,6 @@ class CarState(CarStateBase):
     self.das_control = copy.copy(cp_ap_party.vl["DAS_control"])
 
     return ret, ret_sp
-
-  def update_handsOnDisengage(self, cruise_state):
-    # dynamic hands on level to disable.
-    # L3: blinkers or standstill
-    # L2: recent cruise engagement or road noise
-    # L1: normal driving only if nothing else prevents it
-    standstill = cruise_state == "STANDSTILL"
-
-    if self.blinkers or standstill:
-      min_level = 3
-    elif self.c_frame <= 100 or self.road_noise:
-      min_level = 2
-    else:
-      min_level = 1
-
-    return self.hands_on_level >= 3
-
-  def cruise_state_speed(self, cruise_speed, vego):
-    # pull max ACC speed up by 2mph for the first 0.5s of engagement when within 1mph of max speed
-    # prevents longitudenal jerk with OP long
-    if self.gasPressed and not self.cruise_enabled_prev:
-      self.speedGate = True
-    elif not self.gasPressed:
-      if self.speedGate:
-        self.c_frame = 0
-      self.speedGate = False
-
-    if self.CP.openpilotLongitudinalControl and \
-      (self.speedGate or self.c_frame <= 50) and (cruise_speed - vego) <= 0.5:
-      return vego + 2
-    else:
-      return cruise_speed
 
   @staticmethod
   def get_can_parsers(CP, CP_SP):
